c64abacc351e49583f9994c5dcee7c1bb4b9784d
vajrakumar <vajrakumar.d@gmail.com>
2017-03-16 15:58:20 -0400

    Loading the grid through URL so that it can be mocked in unit tests

--- stc-demo/app/store/Personnel.js
+++ stc-demo/app/store/Personnel.js
@@ -7,15 +7,25 @@ Ext.define('STCDemo.store.Personnel', {
         'name', 'email', 'phone'
     ],
 
-    data: { items: [
-        { name: 'Jean Luc', email: "jeanluc.picard@enterprise.com", phone: "555-111-1111" },
-        { name: 'Worf',     email: "worf.moghsson@enterprise.com",  phone: "555-222-2222" },
-        { name: 'Deanna',   email: "deanna.troi@enterprise.com",    phone: "555-333-3333" },
-        { name: 'Data',     email: "mr.data@enterprise.com",        phone: "555-444-4444" }
-    ]},
-
+    /* data: { items: [
+         { name: 'Jean Luc', email: "jeanluc.picard@enterprise.com", phone: "555-111-1111" },
+         { name: 'Worf',     email: "worf.moghsson@enterprise.com",  phone: "555-222-2222" },
+         { name: 'Deanna',   email: "deanna.troi@enterprise.com",    phone: "555-333-3333" },
+         { name: 'Data',     email: "mr.data@enterprise.com",        phone: "555-444-4444" }
+     ]},
+     proxy: {
+         type: 'memory',
+         reader: {
+             type: 'json',
+             rootProperty: 'items'
+         }
+     }*/
+     
+    autoLoad: true,
     proxy: {
-        type: 'memory',
+        type: 'ajax',
+        url: 'data/personnel.json',
+
         reader: {
             type: 'json',
             rootProperty: 'items'
